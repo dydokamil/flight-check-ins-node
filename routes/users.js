a298e43e1dec05102e152069d3338cfba4f6e004
dydokamil <dydokamil@github.com>
2018-04-28 22:00:20 +0200

    Fixed error messages for user creation

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,23 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource')
 })
 
-router.post('/', (req, res) => {
-  if (!('email' in req.body)) res.json('Provide `email`.')
-  else if (!('password' in req.body)) res.json('Provide `password`.')
-  else {
+router.post('/', async (req, res) => {
+  if (!('email' in req.body) || req.body.email.length === 0) {
+    res.status(400).json({ error: 'Provide `email`.' })
+  } else if (!('password' in req.body) || req.body.password.length === 0) {
+    res.status(400).json({
+      error: 'Provide `password`.'
+    })
+  } else {
     // res.json(User.createUser(email, password))
     const { email, password } = req.body
-    User.createUser(email, password)
-      .then(response => res.json(response))
-      .catch(err => res.json(err))
+    const result = await User.createUser(email, password)
+    // check if duplicate error code
+    if (result.code === 11000) {
+      res.status(400).json({ error: 'This email is already taken!' })
+    } else {
+      res.json(result).catch(err => res.status(500).json(err))
+    }
   }
 })
 
