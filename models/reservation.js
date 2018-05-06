8a3e29a0ca0ce4ea74a8f5280f2de588118430c8
dydokamil <dydokamil@gmail.com>
2018-05-06 18:44:58 +0200

    Various fixes

--- models/reservation.js
+++ models/reservation.js
@@ -1,87 +1,87 @@
-const mongoose = require('mongoose')
-const moment = require('moment')
+const mongoose = require("mongoose")
+const moment = require("moment")
 
-const { BASE_PRICE, CHECK_IN_FEE } = require('../consts')
-const User = require('./user')
-const Seat = require('./seat')
+const { BASE_PRICE, CHECK_IN_FEE } = require("../consts")
+const User = require("./user")
+const Seat = require("./seat")
 
 const Schema = mongoose.Schema
 
 const ReservationSchema = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
-  seat: { type: Schema.Types.ObjectId, ref: 'Seat' },
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  seat: { type: Schema.Types.ObjectId, ref: "Seat" },
   reservedUntil: { type: Date },
   paid: { type: Schema.Types.Boolean, default: false },
-  price: { type: Schema.Types.Number }
+  price: { type: Schema.Types.Number },
 })
 
-ReservationSchema.statics.getSeatsAndReservations = async function (token) {
+ReservationSchema.statics.getSeatsAndReservations = async function(token) {
   let reservedSeats = await this.find({
-    reservedUntil: { $gte: moment.utc() }
+    reservedUntil: { $gte: moment.utc() },
   })
-    .select('seat -_id')
+    .select("seat -_id")
     .lean()
 
-  reservedSeats = reservedSeats.map(reservedSeat => reservedSeat.seat)
+  reservedSeats = reservedSeats.map((reservedSeat) => reservedSeat.seat)
   reservedSeats = await Seat.find({
-    _id: { $in: reservedSeats }
+    _id: { $in: reservedSeats },
   })
 
   let notReservedSeats = await Seat.find({
     _id: {
-      $nin: reservedSeats
-    }
+      $nin: reservedSeats,
+    },
   }).lean()
 
-  notReservedSeats = notReservedSeats.map(notReservedSeat => ({
+  notReservedSeats = notReservedSeats.map((notReservedSeat) => ({
     ...notReservedSeat,
-    available: true
+    available: true,
   }))
 
   return { reservedSeats, notReservedSeats }
 }
 
-ReservationSchema.statics.makeRandomReservation = async function (email) {
+ReservationSchema.statics.makeRandomReservation = async function(email) {
   const { notReservedSeats } = await this.getSeatsAndReservations()
 
   const seatId = Math.round(Math.random() * notReservedSeats.length - 1)
 
   return new Promise((resolve, reject) => {
     this.makeReservation(email, seatId, true)
-      .then(res => resolve(res))
-      .catch(err => reject(err))
+      .then((res) => resolve(res))
+      .catch((err) => reject(err))
   })
 }
 
-ReservationSchema.statics.makeReservation = async function (
+ReservationSchema.statics.makeReservation = async function(
   email,
   seatId,
-  discount = false
+  discount = false,
 ) {
   // check if this user has already made a reservation
   const user = await User.findOne({ email })
-  if (!user) return { error: 'User not found.' }
+  if (!user) return { error: "User not found." }
   const reservationForUser = await this.findOne({
     user,
     reservedUntil: {
-      $gte: moment.utc()
-    }
+      $gte: moment.utc(),
+    },
   })
   if (reservationForUser) {
-    throw new Error('You have already made a reservation.')
+    throw new Error("You have already made a reservation.")
   }
   const seat = await Seat.findOne({ id: seatId })
-  if (!seat) throw new Error('Seat not found!')
+  if (!seat) throw new Error("Seat not found!")
 
   // check if this seat has already been reserved
   const reservationForSeat = await this.findOne({
     seat,
     reservedUntil: {
-      $gte: moment.utc()
-    }
+      $gte: moment.utc(),
+    },
   })
   if (reservationForSeat) {
-    throw new Error('This seat is already reserved.')
+    throw new Error("This seat is already reserved.")
   }
 
   // create a new reservation insance
@@ -94,25 +94,25 @@ ReservationSchema.statics.makeReservation = async function (
     user,
     price,
     seat: seatToReserve,
-    reservedUntil: moment.utc().add(3, 'minutes')
+    reservedUntil: moment.utc().add(3, "minutes"),
   })
 
-  return reservation.save().then(res => ({
+  return reservation.save().then((res) => ({
     _id: res._id,
     paid: res.paid,
     price: res.price,
     reservedUntil: res.reservedUntil,
-    seat: res.seat
+    seat: res.seat,
   }))
 }
 
-ReservationSchema.statics.removeAllReservations = function () {
+ReservationSchema.statics.removeAllReservations = function() {
   return this.remove({})
 }
 
-ReservationSchema.statics.cancelReservation = async function (email) {
+ReservationSchema.statics.cancelReservation = async function(email) {
   const user = await User.findOne({ email })
-  if (!user) throw new Error('User not found!')
+  if (!user) throw new Error("User not found!")
 
   const reservation = await this.findOne({ user })
   const seat = await Seat.findById(reservation.seat)
@@ -121,4 +121,4 @@ ReservationSchema.statics.cancelReservation = async function (email) {
   return seat
 }
 
-module.exports = mongoose.model('Reservation', ReservationSchema)
+module.exports = mongoose.model("Reservation", ReservationSchema)
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,28 @@
-var express = require('express')
+var express = require("express")
 var router = express.Router()
-var jwt = require('jsonwebtoken')
-const fs = require('fs')
-const path = require('path')
+var jwt = require("jsonwebtoken")
+const fs = require("fs")
+const path = require("path")
 
-const User = require('../models/user')
+const User = require("../models/user")
 
 const privateKey = fs.readFileSync(
-  path.join(__dirname, '../keys/private_key.pem')
+  path.join(__dirname, "../keys/private_key.pem"),
 )
 
 // singup route
-router.post('/', async (req, res) => {
-  if (!Object.keys(req.body).includes('email') || req.body.email.length === 0) {
+router.post("/", async (req, res) => {
+  console.log(req.body)
+  if (!Object.keys(req.body).includes("email") || req.body.email.length === 0) {
     res.status(400).json({
-      error: 'Provide `email`.'
+      error: "Provide `email`.",
     })
   } else if (
-    !Object.keys(req.body).includes('password') ||
+    !Object.keys(req.body).includes("password") ||
     req.body.password.length === 0
   ) {
     res.status(400).json({
-      error: 'Provide `password`.'
+      error: "Provide `password`.",
     })
   } else {
     // res.json(User.createUser(email, password))
@@ -29,7 +30,7 @@ router.post('/', async (req, res) => {
     const result = await User.createUser(email, password)
     // check if duplicate error code
     if (result.code === 11000) {
-      res.status(400).json({ error: 'This email is already taken!' })
+      res.status(400).json({ error: "This email is already taken!" })
     } else {
       const token = await generateToken(result.email)
       res.json({ token, email })
@@ -37,35 +38,35 @@ router.post('/', async (req, res) => {
   }
 })
 
-function generateToken (email) {
+function generateToken(email) {
   return new Promise((resolve, reject) => {
-    const sign = jwt.sign({ email }, privateKey, { algorithm: 'RS256' })
+    const sign = jwt.sign({ email }, privateKey, { algorithm: "RS256" })
     resolve(sign)
   })
 }
 
 // login route
-router.post('/login', async (req, res) => {
-  if (!Object.keys(req.body).includes('email') || req.body.email.length === 0) {
-    res.status(400).json({ error: 'Provide `email`.' })
+router.post("/login", async (req, res) => {
+  if (!Object.keys(req.body).includes("email") || req.body.email.length === 0) {
+    res.status(400).json({ error: "Provide `email`." })
   } else if (
-    !Object.keys(req.body).includes('password') ||
+    !Object.keys(req.body).includes("password") ||
     req.body.password.length === 0
   ) {
     res.status(400).json({
-      error: 'Provide `password`.'
+      error: "Provide `password`.",
     })
   } else {
     const { email, password } = req.body
     const user = await User.findOne({ email })
 
     if (!user) {
-      res.status(404).json({ error: 'Email not found.' })
+      res.status(404).json({ error: "Email not found." })
     } else {
       const same = await user.comparePassword(password)
 
       if (!same) {
-        res.status(401).json({ error: 'Password incorrect.' })
+        res.status(401).json({ error: "Password incorrect." })
       } else {
         // generate token
         const token = await generateToken(user.email)
